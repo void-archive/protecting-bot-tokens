add95199415087b382bb1ff6c4c513c6df8442d2
Dave R <davetheprogrammer2@gmail.com>
2018-08-08 18:15:59 +0100

    one more

--- for_discord.js/index.js
+++ for_discord.js/index.js
@@ -6,7 +6,7 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', async msg => {
+client.on('message', msg => {
     if (msg.content === 'ping') {
         msg.reply('pong');
     } 
